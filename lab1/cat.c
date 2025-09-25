331210d065d621d0254d02dc67b61c0a1cf866da
eyevievv <sokol28.nik@gmail.com>
2025-09-25 13:58:01 +0300

    Fixed mycat

--- lab1/cat.c
+++ lab1/cat.c
@@ -1,67 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-void print_visible(unsigned char c);
-
-void cat() {
-
-
-}
-
-void print_visible(unsigned char c) {
-    if (c >= 32 && c <= 126) {
-        putchar(c); 
-    } else if (c == '\n' || c == '\t' || c == '\r') {
-        putchar(c); 
-    } else if (c < 32) {
-        printf("^%c", c + 64);
-    } else if (c == 127) {
-        printf("^?");
-    } else {
-        printf("M-");
-        print_visible(c - 128);
-    }
-}
-
-
-void command_b(char* buffer, int* strCnt) {
-    if((buffer[0] == '\0') || (buffer[0] == '\n')) {
-        fprintf(stdout, "\n");
-    }
-    else {
-        ++strCnt;
-        fprintf(stdout, "    %d  %s", *strCnt, buffer);
-    }
-}
+#define BUF_SIZE 1024
 
 void command_n(char* buffer, int* strCnt) {
-    ++strCnt;
-    fprintf(stdout, "    %d  %s", *strCnt, buffer);
+    char tmp[BUF_SIZE];
+
+    ++(*strCnt);
+    snprintf(tmp, sizeof(tmp), "%6d    %s", *strCnt, buffer);
+
+    strcpy(buffer, tmp);
+}
+
+void command_b(char* buffer, int* strCnt) {
+    char tmp[BUF_SIZE];
+
+    if((buffer[0] == '\0') || (buffer[0] == '\n')) {
+        snprintf(tmp, sizeof(tmp), "\n");
+    }
+    else {
+        command_n(buffer, strCnt);
+    }
+    
 }
 
 void command_E(char* buffer) {
+    char tmp[BUF_SIZE];
     if (strstr(buffer, "\n") != NULL) {
         buffer[strcspn(buffer, "\n")] = '\0';
-        fprintf(stdout, "%s$\n", buffer);
+        snprintf(tmp, sizeof(tmp), "%s$\n", buffer);
     }
     else {
-        fprintf(stdout, "%s", buffer);
+        snprintf(tmp, sizeof(tmp), "%s\n", buffer);
     }
+    strcpy(buffer, tmp);
 }
 
-void command_v(char* buffer) {
-    for (size_t i = 0; i < strlen(buffer); ++i) {
-        print_visible(buffer[i]);
-    }
+int checkFlag(char* flags, char fl) {
+    return strchr(flags, fl) != NULL;
 }
-			
-void command_T(char* buffer) {
-    for (size_t i = 0; i < strlen(buffer); ++i) {
-        if (buffer[i] == '\t') {
-            fprintf(stdout, "^I");
+
+void cat(int argc, char** argv) {
+    char buffer[BUF_SIZE];
+    char flags[100];
+    char line_flags[BUF_SIZE];
+    int line_flagsCount = 0;
+    int flagsCount = 0;
+    char * filename;
+
+    if (argc == 1) {
+        while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+            fprintf(stdout, "%s", buffer);
+        }
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        if (strstr(argv[i], "--") != NULL) {
+            line_flags[line_flagsCount++] = argv[i];
+        }
+        else if (argv[i][0] == '-') {
+            for (size_t j = 1; j < strlen(argv[i]); ++j) {
+                flags[flagsCount++] = argv[i][j];
+            }
         }
         else {
-            fprintf(stdout, "%c", buffer[i]);
+            filename = argv[i];
         }
     }
+
+    FILE * file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stdout, "cat: %s", filename);
+        exit(1);
+    }
+    
+    /// lines parsing
+    int strCnt = 0;
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        if (flagsCount != 0) {
+            if (checkFlag(flags, 'b')) {
+                command_b(buffer, &strCnt);
+            }
+            else if (checkFlag(flags, 'n') || checkFlag(flags, "number")) {
+                command_n(buffer, &strCnt);
+            }
+
+            if (checkFlag(flags, 'E')) {
+                command_E(buffer);
+            }
+        }
+        fprintf(stdout, "%s", buffer);
+    }
+    fclose(file);
+    
 }
