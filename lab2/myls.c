404beb10c132948d311c3bd29566eca25738c3d9
eyevievv <sokol28.nik@gmail.com>
2025-10-02 13:24:14 +0300

    lab2: solved colorizing and permission error problems

--- lab2/myls.c
+++ lab2/myls.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <errno.h>
 
 #define GREEN_COLOR "\033[32m"
 #define BLUE_COLOR "\033[34m"
@@ -19,8 +20,20 @@
 #define BOLD "\033[1m"
 
 #define BUF_SIZE 4096
+#define CAPACITY 128
 
 #define PERMS_SIZE 11
+
+typedef struct {
+    char path[BUF_SIZE];
+    int errnum;
+} Errors;
+
+static int cmp_strptr(const void *p1, const void *p2) {
+    const char *a = *(const char * const *)p1;
+    const char *b = *(const char * const *)p2;
+    return strcmp(a, b);
+}
 
 void ls(int* a_flag, int* l_flag, const char** path);
 void handle_params(int argc, char **argv, int* a_flag, int* l_flag, const char** path);
@@ -71,39 +84,101 @@ void ls(int* a_flag, int* l_flag, const char** path) {
     struct dirent* entry;
     struct stat st;
 
+    char** files = malloc(CAPACITY * sizeof(char*)); 
+    if (!files) { 
+        perror("malloc"); 
+        exit(1); 
+    }
+    int files_count = 0;
+
+    Errors* errors = malloc(CAPACITY * sizeof(Errors));
+    if (!errors) { 
+        perror("errors"); 
+        exit(1); 
+    }
+    int err_count = 0;
+
     DIR* dir = opendir(*path);
     if (!dir) {
-        perror("Cannot open dir");
-        printf("path: %s", *path);
-        exit(1);
+        fprintf(stderr, "ls: cannot open directory '%s': %s\n", *path, strerror(errno));
+        return;
     }
 
-    while ((entry = readdir(dir)) != NULL) {
-        snprintf(fullpath, sizeof(fullpath), "%s/%s", *path, entry->d_name);
+    while ((entry = readdir(dir)) != NULL) {        
+        files[files_count] = malloc(strlen(entry->d_name) + 1);
         
-        if (!*a_flag && entry->d_name[0] == '.') {
+        if (files[files_count] != NULL) {
+            strcpy(files[files_count], entry->d_name);
+        }
+        files_count++;
+    }
+    
+    qsort(files, files_count, sizeof files[0], cmp_strptr);
+
+    // find errors
+    for (int i = 0; i < files_count; ++i) {
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", *path, files[i]);
+
+        if (lstat(fullpath, &st) == -1) {
+            errors[err_count].errnum = errno;
+            strncpy(errors[err_count].path, fullpath, BUF_SIZE-1);
+            errors[err_count].path[BUF_SIZE-1] = '\0';
+
+            err_count++;
+        }
+    }
+
+    // print errors
+    for (int i = 0; i < err_count; i++) {
+        fprintf(stderr, "ls: %s: %s\n", errors[i].path, strerror(errors[i].errnum));
+    }
+
+    // for table_view
+    int max_len = 0;
+    for (int i = 0; i < files_count; i++) {
+        int len = strlen(files[i]);
+        if (len > max_len) max_len = len;
+    }
+
+    // print files
+    for (int i = 0; i < files_count; ++i) {
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", *path, files[i]);
+        
+        if (!*a_flag && files[i][0] == '.') {
             continue;
         }
         
         // init struct stat
         if (lstat(fullpath, &st) == -1) {
-            perror("Cannot init stat struct");
-            exit(1);
+            if (*l_flag) {
+                printf("??????????\t ?\t ?\t ?\t ?\t ?    %s\n", files[i]);
+            }
+            free(files[i]);
+            continue;
         }
 
         if (*l_flag) {
-            full_format(st, entry->d_name);
+            full_format(st, files[i]);
         }
         else {
-            colorize_filename(st, entry->d_name);
+            colorize_filename(st, files[i]);
+            printf("%-*s", max_len + 2, ""); // выравнивание после имени
+
+            if ((i+1) % 5 == 0) printf("\n"); // 5 колонок
+
         }
+        free(files[i]);
     }
 
     if (!*l_flag) {
         printf("\n");
     }
+    
+    free(files);
+    free(errors);
     closedir(dir);
 }
+
 
 void colorize_filename(struct stat st, const char* name) {
     if (S_ISREG(st.st_mode)) { // regular file
@@ -113,7 +188,12 @@ void colorize_filename(struct stat st, const char* name) {
         printf(LIGHT_BLUE_COLOR BOLD "%s  " RESET_COLOR, name);
     }
     else if (S_ISDIR(st.st_mode)) { // directory
-        printf(BLUE_COLOR GREEN_BACK "%s" RESET_COLOR "  ", name);
+        if (st.st_mode & S_IWOTH) {
+            printf(BLUE_COLOR GREEN_BACK "%s" RESET_COLOR "  ", name);
+        }
+        else {
+            printf(BLUE_COLOR BOLD "%s" RESET_COLOR "  ", name);
+        }
     }
     else {
         printf("%s  ", name);
@@ -149,14 +229,14 @@ void add_time(struct stat st) {
 void add_user(struct stat st) {
     struct passwd* pwd_info = getpwuid(st.st_uid);
     if (pwd_info == NULL) {
-        perror("Error pwd_info");
-        exit(1);
+        printf("? "); // pwd_info error
+        return;
     }
     printf("%s ", pwd_info->pw_name);
 }
 
 void add_permissions(mode_t mode) {
-    char perms[PERMS_SIZE];
+    char perms[PERMS_SIZE] = "----------";
 
     if (S_ISREG(mode)) { // regular file
         perms[0] = '-';
@@ -178,12 +258,13 @@ void add_permissions(mode_t mode) {
     }
 
     // find permissions
-    int target_perm = S_IRUSR;
-    for (int i = 0; i < 9; target_perm >>= 1, ++i) {
-        perms[i+1] = (mode & target_perm) ? "rwx"[i%3] : '-';
-    } 
-    perms[10] = '\0';
-
+    if (mode != 0) {
+        int target_perm = S_IRUSR;
+        for (int i = 0; i < 9; target_perm >>= 1, ++i) {
+            perms[i+1] = (mode & target_perm) ? "rwx"[i%3] : '-';
+        } 
+        perms[10] = '\0';
+    }
     // add permissions to out
     printf("%s ", perms);
 }
@@ -196,8 +277,8 @@ void add_group(struct stat st) {
     struct group* grp_info = getgrgid(st.st_gid);
 
     if (grp_info == NULL) {
-        perror("Error grp_info");
-        exit(1);
+        printf("? "); // grp_info error
+        return;
     }
     printf("%s ", grp_info->gr_name);
 }
