ad5e0f87aa916b9d1b6788eccdc8f19db156397e
eyevievv <sokol28.nik@gmail.com>
2025-10-15 21:18:50 +0300

    lab4: implemented and tested ugoa perms adding

--- lab4/mychmod.c
+++ lab4/mychmod.c
@@ -9,15 +9,23 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define ADD_MASK 0
+#define SUBTRACT_MASK 1
+#define ASSIGN_MASK 2
+
 void processMask(const char* filename, const char* mask_str);
 
 void processAsBitMask(const char* filename, const char* mask_str);
 void processAsLetterMask(const char* filename, const char* mask_str);
 
-// void checkAndSetMode(const char* file);
 void checkDigitMask(const char* number);
-void checkLetterMask(const char* letters);
+int checkLetterMaskAndDefineOperation(const char* letters);
 
+void combine(mode_t* initMask, mode_t newMask, int operation);
+void changeMod(const char* filename, mode_t initMask);
+
+void getCurrentFilePermissions(const char* filename, struct stat* st);
+int getOperation(const char letter);
 void onExitHandler(int exit_code, void* arg);
 
 int main(int argc, char** argv) {
@@ -26,12 +34,13 @@ int main(int argc, char** argv) {
         return 1;
     }
     
-    const char* filename = argv[2]; 
     const char* mask = argv[1];
-
     on_exit(onExitHandler, argv[1]);
-    processMask(filename, mask);
 
+    for (int i = 2; i < argc; ++i) { 
+        const char* filename = argv[2]; 
+        processMask(filename, mask);
+    }
     return 0;
 }
 
@@ -50,60 +59,54 @@ void processMask(const char* filename, const char* mask_str) {
 
 void processAsBitMask(const char* filename, const char* mask_str) {
     checkDigitMask(mask_str);
+
+    struct stat st;
+    getCurrentFilePermissions(filename, &st);
+    
+    mode_t initMask = st.st_mode;
     
     char* endptr;
-    long mask = strtol(mask_str, &endptr, 8);
+    long newMask = strtol(mask_str, &endptr, 8);
     if (*endptr != '\0') {
-        exit(3);
-    }
-
-    if (chmod(filename, (mode_t)mask) == -1) {
         exit(2);
     }
 
+    combine(&initMask, newMask, -1);
+    changeMod(filename, (mode_t)initMask);
 }
 
 void processAsLetterMask(const char* filename, const char* mask_str) {
-    checkLetterMask(mask_str);
+    int operation = checkLetterMaskAndDefineOperation(mask_str);
     
-    mode_t mask = 0;
+    struct stat st;
+    getCurrentFilePermissions(filename, &st);
+
+    mode_t initMask = st.st_mode;
+    mode_t newMask = 0;
+
     for (size_t i = 0; i < strlen(mask_str); ++i) {
         switch (mask_str[i]) {
-            case 'u':
-                mask |= S_IRUSR | S_IWUSR | S_IXUSR;
-                break;
-            case 'w':
-                mask |= S_IRGRP | S_IWGRP | S_IXGRP;
-                break;
-            case 'o':
-                mask |= S_IROTH | S_IWOTH | S_IXOTH;
-                break;
-            case 'a':
-                mask |= S_IRUSR | S_IWUSR | S_IXUSR;
-                mask |= S_IRGRP | S_IWGRP | S_IXGRP;
-                mask |= S_IROTH | S_IWOTH | S_IXOTH;
-                break;
-            default:
-                break;
-            }
-    }
-    
-    if (chmod(filename, mask) == -1) {
-        exit(2);
+        case 'u':
+            newMask = S_IRUSR | S_IWUSR | S_IXUSR;
+            break;
+        case 'g':
+            newMask |= S_IRGRP | S_IWGRP | S_IXGRP;
+            break;
+        case 'o':
+            newMask |= S_IROTH | S_IWOTH | S_IXOTH;
+            break;
+        case 'a':
+            newMask |= S_IRUSR | S_IWUSR | S_IXUSR;
+            newMask |= S_IRGRP | S_IWGRP | S_IXGRP;
+            newMask |= S_IROTH | S_IWOTH | S_IXOTH;
+            break;
+        default:
+            break;
+        }
     }
 
-}
-
-void checkAndSetMode(const char* file) {
-    mode_t mask = 0;
-    mask |= S_IRUSR | S_IWUSR | S_IXUSR;
-    mask |= S_IRGRP | S_IWGRP | S_IXGRP;
-    mask |= S_IROTH | S_IWOTH | S_IXOTH;
-
-    
-    if (chmod(file, mask) == -1) {
-        exit(2);
-    }
+    combine(&initMask, newMask, operation);    
+    changeMod(filename, initMask);
 }
 
 void checkDigitMask(const char* number) {
@@ -115,15 +118,73 @@ void checkDigitMask(const char* number) {
     }
 }
 
-void checkLetterMask(const char* letters) {
+int checkLetterMaskAndDefineOperation(const char* letters) {
+    int res = -1;
     for (size_t i = 0; i < strlen(letters); ++i) {
         if (!strchr("ugoarwx+-=", letters[i])) {
             exit(1);
         }
+        if (strchr("+-=", letters[i])) {
+            if (res != -1) exit(1);
+            res = getOperation(letters[i]);
+        }
+    }
+    return res;
+}
+
+void combine(mode_t* initMask, mode_t newMask, int operation) {
+    if (operation == -1) {
+        *initMask = newMask;
+    }
+    switch (operation)
+    {
+    case ADD_MASK:
+        *initMask |= newMask;
+        break;
+    case SUBTRACT_MASK:
+        *initMask &= ~newMask;
+        break;
+    case ASSIGN_MASK:
+        *initMask = newMask;
+        break;    
+    default:
+        break;
     }
 }
 
-void onExitHandler(int exit_code, void* arg) {
+void changeMod(const char *filename, mode_t initMask) {
+    if (chmod(filename, initMask) == -1) {
+        exit(2);
+    }
+}
+
+void getCurrentFilePermissions(const char* filename, struct stat* st) {
+    if (stat(filename, st) == -1) {
+        perror("mychmod error");
+        exit(2);
+    }
+}
+
+int getOperation(const char letter) {
+    int res = -1;
+    switch (letter) {
+        case '+':
+            res = ADD_MASK;
+            break;
+        case '-':
+            res = SUBTRACT_MASK;
+            break;
+        case '=':
+            res = ASSIGN_MASK;
+            break;                
+        default:
+            break;
+    }
+    return res;
+}
+
+void onExitHandler(int exit_code, void *arg)
+{
     switch (exit_code) {
         case 0:
             return;
@@ -133,7 +194,7 @@ void onExitHandler(int exit_code, void* arg) {
             break;
         default:
             int error = errno;
-            fprintf(stderr, "Error: %d (%s)\n", error, stderror(error));
+            fprintf(stderr, "Error: %d (%s)\n", error, strerror(error));
             break;
     }
 }
